028426a6e9e0b799397b81d6f981f598c4af9697
nintichance <mintidance@gmail.com>
2024-06-08 14:20:04 -0400

    Add error handling to check if the user input is valid

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var dataType string
+	var dataTypes []string
+	dsString := "linkedlist stack queue bst minheap hash trie"
+	dataTypes = strings.Fields(dsString)
+
 	fmt.Println("welcome to ninti's data structures in go!")
 	fmt.Println("which data structure would you like to see?")
-	fmt.Println("choose one: linked list, stack, queue, bst, priority queue, hash, trie")
+	fmt.Println("choose one: linkedlist, stack, queue, bst, minheap, hash, trie")
 	fmt.Scan(&dataType)
+
+	for !searchSlice(dataType, dataTypes) {
+		fmt.Printf("Your input: %v is invalid. Please try again:", dataType)
+		fmt.Scan(&dataType)
+	}
+
 	fmt.Printf("Wonderful! You chose: %v", dataType)
+}
+
+func searchSlice(str string, arr []string) bool {
+	var found bool = false
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == str {
+			found = true
+			break
+		}
+	}
+	return found
 }
