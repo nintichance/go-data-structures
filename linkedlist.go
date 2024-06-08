a0aaed4ddc075885446fdf37bff29bb5686c0c21
nintichance <mintidance@gmail.com>
2024-06-08 15:29:28 -0400

    Add functionality for LinkedList and its methods size, insert, print, printReverse, reverse, and reverseRecursive. reverseRecursive is broken.

---
+++ linkedlist.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Val  uint
+	Next *Node
+}
+
+type LinkedList struct {
+	Head *Node
+}
+
+func (l *LinkedList) init(val uint32) {
+	l.Head = &Node{
+		Val: uint(val),
+	}
+}
+
+func (l *LinkedList) size() uint {
+	var count uint
+	node := l.Head
+
+	for node != nil {
+		count += 1
+		node = node.Next
+	}
+	return count
+}
+
+func (l *LinkedList) insert(val uint32) {
+	newNode := Node{
+		Val: uint(val),
+	}
+
+	head := l.Head
+
+	for head.Next != nil {
+		head = head.Next
+	}
+	head.Next = &newNode
+}
+
+func (l *LinkedList) print(n *Node) {
+	if n == nil {
+		return
+	}
+	fmt.Printf("PRINTED %v\n", n.Val)
+	l.print(n.Next)
+}
+
+func (l *LinkedList) printReverse(n *Node) {
+	if n == nil {
+		return
+	}
+	l.printReverse(n.Next)
+	fmt.Printf("PRINTED %v\n", n.Val)
+}
+
+func (l *LinkedList) reverse() {
+	curr := l.Head
+	var prev *Node
+	var next *Node
+
+	for curr != nil {
+		next = curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	l.Head = prev
+}
+
+func (l *LinkedList) reverseRecursive(n *Node) *Node {
+	if n == nil || n.Next == nil {
+		return n
+	}
+	newHead := l.reverseRecursive(n.Next)
+	n.Next.Next = n
+	n.Next = nil
+	return newHead
+}
--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 )
 
 func main() {
+
+	dt, dts := getDataType()
+	for !searchSlice(dt, dts) {
+		fmt.Printf("Your input: %v is invalid. Please try again:\n", dt)
+		fmt.Scan(&dt)
+	}
+
+	fmt.Printf("Wonderful! You chose: %v\n", dt)
+
+	if dt == "linkedlist" {
+		runLinkedList()
+	}
+}
+
+func getDataType() (string, []string) {
 	var dataType string
 	var dataTypes []string
 	dsString := "linkedlist stack queue bst minheap hash trie"
@@ -15,15 +30,8 @@ func main() {
 	fmt.Println("which data structure would you like to see?")
 	fmt.Println("choose one: linkedlist, stack, queue, bst, minheap, hash, trie")
 	fmt.Scan(&dataType)
-
-	for !searchSlice(dataType, dataTypes) {
-		fmt.Printf("Your input: %v is invalid. Please try again:", dataType)
-		fmt.Scan(&dataType)
-	}
-
-	fmt.Printf("Wonderful! You chose: %v", dataType)
+	return dataType, dataTypes
 }
-
 func searchSlice(str string, arr []string) bool {
 	var found bool = false
 	for i := 0; i < len(arr); i++ {
@@ -33,4 +41,23 @@ func searchSlice(str string, arr []string) bool {
 		}
 	}
 	return found
+}
+
+func runLinkedList() {
+	l := &LinkedList{}
+	l.init(1)
+	l.insert(2)
+	l.insert(3)
+	l.insert(4)
+	fmt.Printf("The size is: %v\n", l.size())
+	l.print(l.Head)
+	fmt.Println("##############")
+	l.printReverse(l.Head)
+	fmt.Println("REVERSE ##############")
+	l.reverse()
+	l.print(l.Head)
+	fmt.Println("REVERSE RECURSIVE ##############")
+	l.reverseRecursive(l.Head)
+	l.print(l.Head)
+	fmt.Println("##############")
 }
